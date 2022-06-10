879914ce9a0886ddd1703288447e411830c6a582
Chitrank Samaiya <Chitrank_Samaiya@external.mckinsey.com>
2022-06-11 02:11:33 +0530

    0. Scope for reporting for average skill rating above 3 1. Scope for reporting for average skill rating above 3 and no rating below 2

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,16 @@
 class Candidate < User
 
+  # --------- Associations -------------------------------------------------
+  has_many :interviews, dependent: :destroy
+
   # --------- Validations --------------------------------------------------
   validates :name, presence: true, length: {maximum: 16}
+
+  # --------- Scopes --------------------------------------------------
+  scope :avg_skill_ratings_above_3, ->(job_id) {select(:id, :name, :email).joins(interviews: [:job, :skill_ratings])
+                                                                   .where("skill_ratings.rating >= 2 and interviews.job_id =?", job_id).having("avg(skill_ratings.rating) > 3")
+                                                                   .group(:id)}
+  scope :avg_skill_ratings_above_3_and_no_rating_below_2, ->(job_id) {select(:id, :name, :email).joins(interviews: [:job, :skill_ratings])
+                                                                   .where("skill_ratings.rating >= 2 and interviews.job_id =?", job_id).having("avg(skill_ratings.rating) > 3")
+                                                                   .group(:id)}
 end
