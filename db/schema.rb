2087f20ddc34e3c13b04ab3517fb64ebe678dcca
Chitrank Samaiya <Chitrank_Samaiya@external.mckinsey.com>
2022-06-11 02:12:23 +0530

    0. Seed file 1. Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_120745) do
+ActiveRecord::Schema.define(version: 2022_06_10_131714) do
+
+  create_table "interview_skill_ratings", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "interview_id"
+    t.bigint "skill_rating_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["interview_id"], name: "index_interview_skill_ratings_on_interview_id"
+    t.index ["skill_rating_id"], name: "index_interview_skill_ratings_on_skill_rating_id"
+  end
+
+  create_table "interviews", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "candidate_id"
+    t.bigint "employee_id"
+    t.bigint "job_id"
+    t.string "round"
+    t.text "other_attributes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["candidate_id"], name: "index_interviews_on_candidate_id"
+    t.index ["employee_id"], name: "index_interviews_on_employee_id"
+    t.index ["job_id"], name: "index_interviews_on_job_id"
+  end
+
+  create_table "jobs", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "title"
+    t.datetime "published_at"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "skill_mappings", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "skill_id", null: false
+    t.bigint "user_id"
+    t.bigint "skill_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["skill_id"], name: "index_skill_mappings_on_skill_id"
     t.index ["user_id"], name: "index_skill_mappings_on_user_id"
+  end
+
+  create_table "skill_ratings", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "skill_id"
+    t.integer "rating"
+    t.text "other_attributes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["skill_id"], name: "index_skill_ratings_on_skill_id"
   end
 
   create_table "skills", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
@@ -38,6 +77,4 @@ ActiveRecord::Schema.define(version: 2022_06_09_120745) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "skill_mappings", "skills"
-  add_foreign_key "skill_mappings", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+job_1  = Job.create!(title: "Lead Developer", description: "This is posted today", published_at: Time.now)
+job_2 = Job.create!(title: "Senior Developer", description: "This is posted today", published_at: Time.now)
+
+skill_1 = Skill.create!(name: 'ruby')
+skill_2 = Skill.create!(name: 'java')
+
+emp_1 = Employee.create!(name: 'ashok', email: 'ashok@example.com', experience: '6.2', skill_set_attributes: [{id: skill_1.id}, {id: skill_2.id}])
+emp_2 = Employee.create!(name: 'alok', email: 'alok@example.com', experience: '6.5', skill_set_attributes: [{id: skill_1.id}, {id: skill_2.id}])
+
+can_1 = Candidate.create!(name: 'abhinav', email: 'abhinav@example.com', experience: '4.2', skill_set_attributes: [{id: skill_1.id}, {id: skill_2.id}])
+can_2 = Candidate.create!(name: 'abhishek', email: 'abhishek@example.com', experience: '5.2', skill_set_attributes: [{id: skill_1.id}, {id: skill_2.id}])
+
+Interview.create!(candidate_id: can_1.id, employee_id: emp_1.id, job_id: job_1.id, round: "Round 1")
+Interview.create!(candidate_id: can_1.id, employee_id: emp_1.id, job_id: job_1.id, round: "Round 2")
+Interview.create!(candidate_id: can_1.id, employee_id: emp_1.id, job_id: job_1.id, round: "Round 3")
+
+Interview.create!(candidate_id: can_2.id, employee_id: emp_2.id, job_id: job_2.id, round: "Round 1")
+Interview.create!(candidate_id: can_2.id, employee_id: emp_2.id, job_id: job_2.id, round: "Round 2")
+Interview.create!(candidate_id: can_2.id, employee_id: emp_2.id, job_id: job_2.id, round: "Round 3")
